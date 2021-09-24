38451feb91452f01172e4577291cebc827fb8283
Heitor <heitorcsmacedo@gmail.com>
2021-09-24 18:12:37 -0300

    This release contains API Gateways implementation using Ocelot.

---
+++ ApiService/Controllers/ClientController .cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ApiService.Model;
+
+namespace Sqit.CustomerApi.Controllers
+{
+    [Route("service")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        private readonly List<ClientDTO> customers = null;
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetClient(int id)
+        {
+            ClientDTO _client = new ClientDTO();
+            _client.Id = id;
+            _client.Type = (id % 2 == 0 ? "Par" : "Ímpar");
+
+            return Ok(_client);
+        }
+    }
+}
---
+++ ApiService/Model/ClientDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiService.Model
+{
+    public class ClientDTO
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        
+    }
+}
